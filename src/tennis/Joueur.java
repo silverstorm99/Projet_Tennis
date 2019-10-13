fe2b2ea7acb4a306dcd5afe48de71aa6f117f978
SCHMITT <nicolas.schmitt@isen.yncrea.fr>
2019-10-13 16:52:02 +0200

    Ajout de la classe Joueur

---
+++ src/tennis/Joueur.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tennis;
+
+/**
+ * 13/10/2019
+ * @author Nicolas
+ */
+public class Joueur extends Personne{
+    
+}
+
+
+
